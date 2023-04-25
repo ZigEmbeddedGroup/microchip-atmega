3e0d65dc9c17076a67692d59d15a0fe80cda5083
Philipp Wendel <philippwendel@protonmail.com>
2023-04-25 14:49:00 +0200

    Added I2CController init and WriteState

--- src/hals/ATmega328P.zig
+++ src/hals/ATmega328P.zig
@@ -6,6 +6,8 @@ const interfaces = micro.core.experimental;
 
 const peripherals = micro.chip.peripherals;
 const USART0 = peripherals.USART0;
+const I2C = peripherals.TWI;
+const CPU = peripherals.CPU;
 
 pub const cpu = micro.cpu;
 const Port = enum(u8) {
@@ -192,3 +194,125 @@ pub fn Uart(comptime index: usize, comptime pins: interfaces.uart.Pins) type {
         }
     };
 }
+
+// Terminology: I2C instead of TWI (TwoWireInterface) and controller/device, instead of master/slave
+pub fn I2CController(comptime index: usize, comptime pins: interfaces.i2c.Pins) type {
+    // TODO Add config options for pullup and power
+    // TODO Check/Add status codes
+
+    if (index != 0) @compileError("Index is not supported");
+    if (pins.scl != null or pins.sda != null) @compileError("Custom pins are not supported");
+
+    return struct {
+        const Self = @This();
+
+        pub fn init(config: interfaces.i2c.Config) !Self {
+            // "Up to 400kHz data transfer speed" [1, p. 173]
+            if (config.target_speed > 400_000) return interfaces.i2c.InitError.InvalidBusFrequency;
+            // "[Device] operation does not depend on bit rate or prescaler settings,
+            // but the CPU clock frequency in the [device] must be at least 16 times higher than the SCL frequency." [1, p. 180]
+            if (!(interfaces.clock.get().cpu > config.target_speed * 16)) return interfaces.i2c.InitError.InvalidBusFrequency;
+
+            // "[I]nternal pull-ups in the AVR pads can be enabled by setting the PORT bits corresponding to the SCL and SDA pins" [1, p. 179]
+            const enable_internal_pullups = true;
+            if (enable_internal_pullups) {
+                peripherals.PORTC.PORTC |= 0b0011_0000; // SDA = PC4, SCL = PC5
+            }
+
+            // "The PRTWI bit ... must be written to zero to enable the 2-wire serial interface." [1, p. 174]
+            const enable_i2c_power = true;
+            if (enable_i2c_power) {
+                CPU.PRR.modify(.{ .PRTWI = 0 });
+            }
+
+            // SCL_frequency = CPU_Clock_frequency/(16+2(TWBR)*(PrescalerValue)) [1, p. 180]
+            const @"bitrate*prescaler" = (( interfaces.clock.get().cpu / config.target_speed) - 16) / 2;
+            const bitrate_and_prescaler = for ([_]u8{ 1, 4, 16, 64 }, [_]u2{0b00, 0b01, 0b10, 0b11}) |prescaler, raw| {
+                const bitrate = @"bitrate*prescaler" / prescaler;
+                // Bitrate should be at least 10 [2, Sec. Note 5]
+                if (10 <= bitrate and bitrate <= std.math.maxInt(@TypeOf(I2C.TWBR))) break .{ .bitrate = @truncate(u8, bitrate), .prescaler = raw };
+            } else return interfaces.i2c.InitError.InvalidBusFrequency;
+
+            I2C.TWBR = bitrate_and_prescaler.bitrate;
+            I2C.TWSR.modify(.{ .TWPS = .{ .raw = bitrate_and_prescaler.prescaler } });
+            I2C.TWCR.modify(.{ .TWEN = 1 });
+
+            return Self{};
+        }
+
+        pub const WriteState = struct {
+            address: u7,
+
+            pub fn start(address: u7) !WriteState {
+                // Steps 1 - 3 [1, p. 182] and example code at [1, p. 183]
+                I2C.TWCR.modify(.{ .TWINT = 1, .TWSTA = 1, .TWEN = 1 });
+                //debug_print("Waiting for status\r\n", .{});
+                wait_for_i2c_to_finish_operation();
+                return WriteState{ .address = address };
+            }
+
+            pub fn writeAll(self: *WriteState, bytes: []const u8) !void {
+                send(@as(u8, self.address) << 1); // Address is 7 Bit, last bit is read(1)/write(0) [1, p. 175]
+                for (bytes) |b| {
+                    send(b);
+                }
+            }
+
+            pub fn stop(self: *WriteState) !void {
+                _ = self;
+                // example code at [1, p. 183]
+                I2C.TWCR.modify(.{ .TWINT = 1, .TWEN = 1, .TWSTO = 1 });
+            }
+
+            pub fn restart_read(self: *WriteState) !ReadState {
+                try self.send_buffer(0);
+                return ReadState{ .address = self.address };
+            }
+            pub fn restart_write(self: *WriteState) !WriteState {
+                try self.send_buffer(0);
+                return WriteState{ .address = self.address };
+            }
+
+            inline fn send(data: u8) void {
+                I2C.TWDR = data;
+                I2C.TWCR.modify(.{ .TWINT = 1, .TWEN = 1, .TWSTA = 0 });
+                wait_for_i2c_to_finish_operation();
+            }
+
+            inline fn wait_for_i2c_to_finish_operation() void {
+                // "When the [I2C] has finished an operation and expects application response, the TWINT flag is set." [1, p.182]
+                while (I2C.TWCR.read().TWINT == 0) {}
+            }
+        };
+
+        pub const ReadState = struct {
+            address: u7,
+            read_allowed: bool = true,
+
+            pub fn start(address: u7) !ReadState {
+                return ReadState{ .address = address };
+            }
+
+            /// Fails with ReadError if incorrect number of bytes is received.
+            pub fn read_no_eof(self: *ReadState, buffer: []u8) !void {
+                _ = self;
+                _ = buffer;
+            }
+
+            pub fn stop(self: *ReadState) !void {
+                _ = self;
+            }
+
+            pub fn restart_read(self: *ReadState) !ReadState {
+                return ReadState{ .address = self.address };
+            }
+            pub fn restart_write(self: *ReadState) !WriteState {
+                return WriteState{ .address = self.address };
+            }
+        };
+    };
+}
+
+// References:
+// [1] https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+// [2] https://www.nongnu.org/avr-libc/user-manual/group__twi__demo.html
